30dec135bdb8b39503cc97fcbc1ed650de49518e
Micael Jerry <hei.jerry.2@gmail.com>
2024-05-22 11:12:57 +0300

    chore: add argv to list

--- src/main.c
+++ src/main.c
@@ -6,14 +6,37 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 06:13:47 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/05/01 06:25:08 by mfidimal         ###   ########.fr       */
+/*   Updated: 2024/05/22 11:11:56 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(void)
+t_list	*init_list_a(int argc, char const *argv[])
 {
-	ft_printf("Hello world!\n");
-	return 0;
+	t_list	*list_a;
+	int		i;
+	int		*content;
+
+	i = 1;
+	while (i < argc)
+	{
+		content = (int *)malloc(sizeof(int));
+		if (!content)
+			return (NULL);
+		*content = ft_atoi(argv[i]);
+		ft_lstadd_back(&list_a, ft_lstnew(content));
+		i++;
+	}
+	return (list_a);
+}
+
+int	main(int argc, char const *argv[])
+{
+	t_list	*list_a;
+
+	if (argc < 2)
+		return (0);
+	list_a = init_list_a(argc, argv);
+	return (0);
 }
