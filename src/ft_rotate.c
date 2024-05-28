2399ff50f013188a8ba9c7332a1810cb025665a1
Micael Jerry <hei.jerry.2@gmail.com>
2024-05-28 14:37:57 +0300

    fix: update rotate func

--- src/ft_rotate.c
+++ src/ft_rotate.c
@@ -6,39 +6,35 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 13:29:26 by mfidimal          #+#    #+#             */
-/*   Updated: 2024/05/28 13:55:03 by mfidimal         ###   ########.fr       */
+/*   Updated: 2024/05/28 14:38:16 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void	ft_rotate(t_list **list)
+{
+	t_list	*first_element;
+
+	if (!list[0])
+		return ;
+	if (ft_lstsize(list[0]) < 2)
+		return ;
+	first_element = list[0];
+	list[0] = list[0]->next;
+	first_element->next = NULL;
+	ft_lstadd_back(&list[0], first_element);
+}
+
 void	ft_rotate_a(t_list **list_a)
 {
-	t_list	*first_element_a;
-
-	if (!list_a[0])
-		return ;
-	if (ft_lstsize(list_a[0]) < 2)
-		return ;
-	first_element_a = list_a[0];
-	list_a[0] = list_a[0]->next;
-	first_element_a->next = NULL;
-	ft_lstadd_back(&list_a[0], first_element_a);
+	ft_rotate(list_a);
 	ft_printf("ra");
 }
 
 void	ft_rotate_b(t_list **list_b)
 {
-	t_list	*first_element_b;
-
-	if (!list_b[0])
-		return ;
-	if (ft_lstsize(list_b[0]) < 2)
-		return ;
-	first_element_b = list_b[0];
-	list_b[0] = list_b[0]->next;
-	first_element_b->next = NULL;
-	ft_lstadd_back(&list_b[0], first_element_b);
+	ft_rotate(list_b);
 	ft_printf("rb");
 }
 
