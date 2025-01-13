977d0ae30bfaf852dba085881a2ff29cb79053d5
FIDIMALALA Micael Jerry <fidimalala.mj@gmail.com>
2025-01-13 18:31:24 +0300

    chore: update indexation

--- src/ft_sort.c
+++ src/ft_sort.c
@@ -6,11 +6,27 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 15:36:18 by mfidimal          #+#    #+#             */
-/*   Updated: 2025/01/12 16:27:57 by mfidimal         ###   ########.fr       */
+/*   Updated: 2025/01/12 20:47:45 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static int	find_min_index(t_list_int **stack, int diff)
+{
+	t_list_int	*element;
+	int	min;
+
+	element = stack[0];
+	min = element->index;
+	while (element->next)
+	{
+		element = element->next;
+		if((element->index < min) && element->index != diff)
+			min = element->index;
+	}
+	return (min);
+}
 
 void	ft_sort_2(t_list_int **stack_a)
 {
@@ -20,24 +36,28 @@ void	ft_sort_2(t_list_int **stack_a)
 void	ft_sort_3(t_list_int **stack_a)
 {
 	t_list_int	*element;
+	int	min;
+	int	next_min;
 
 	element = stack_a[0];
-	if (element->index == 0 && element->next->index != 1)
+	min = find_min_index(stack_a, -1);
+	next_min = find_min_index(stack_a, min);
+	if (element->index == min && element->next->index != next_min)
 	{
 		ft_rotate_a(stack_a);
 		ft_swap_a(stack_a);
 		ft_reverse_rotate_a(stack_a);
 	}
-	else if (element->index == 1)
+	else if (element->index == next_min)
 	{
-		if (element->next->index == 0)
+		if (element->next->index == min)
 			ft_swap_a(stack_a);
 		else
 			ft_reverse_rotate_a(stack_a);
 	}
 	else
 	{
-		if (element->next->index == 0)
+		if (element->next->index == min)
 			ft_rotate_a(stack_a);
 		else
 		{
--- src/index_util.c
+++ src/index_util.c
@@ -6,7 +6,7 @@
 /*   By: mfidimal <mfidimal@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 22:35:48 by mfidimal          #+#    #+#             */
-/*   Updated: 2025/01/12 10:14:51 by mfidimal         ###   ########.fr       */
+/*   Updated: 2025/01/13 18:29:56 by mfidimal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,14 @@ static t_list_int	*find_next_min(t_list_int **list)
 	t_list_int	*min;
 	t_list_int	*tmp;
 
-	min = list[0];
-	tmp = list[0]->next;
-	while (tmp != NULL)
+	min = NULL;
+	tmp = list[0];
+	while (tmp)
 	{
-		if ((tmp->index == -1) && (tmp->value < min->value))
+		if ((tmp->index == -1) && (!min || tmp->value < min->value))
 			min = tmp;
 		tmp = tmp->next;
 	}
-	if (min->index != -1)
-		return (NULL);
 	return (min);
 }
 
